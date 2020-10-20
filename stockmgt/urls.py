e6fd6fd23b3d68ef6c4d78ddad4ce22f461f2ba7
Mugoya Dihfahsih <dihfahsihm@gmail.com>
2020-10-20 17:56:48 +0300

    work on the urls and the view functions

---
+++ stockmgt/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home')
+    path('items-list', views.list_items, name='list_items')
+]
