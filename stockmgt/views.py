ca51238cf9ef035eae39bf66a91183d98e911aa9
Mugoya Dihfahsih <dihfahsihm@gmail.com>
2020-10-21 15:54:17 +0300

    style up yhe items table

--- stockmgt/views.py
+++ stockmgt/views.py
@@ -13,7 +13,7 @@ def add_item(request):
     form = StockCreateForm(request.POST or None)
     if request.method=='POST':
         if form.is_valid():
-            form.save
+            form.save()
             return redirect('list_items')
     context = {'form':form}
     return render(request, 'add_item.html', context)
