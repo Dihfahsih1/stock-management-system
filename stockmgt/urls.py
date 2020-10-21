d61263e35a7e0e9378b97a75c43287eecc777f5a
Mugoya Dihfahsih <dihfahsihm@gmail.com>
2020-10-21 22:35:02 +0300

    delete item function

--- stockmgt/urls.py
+++ stockmgt/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('items-list', views.list_items, name='list_items'),
     path('add-item', views.add_item, name='add_item'),
     path('add-item/<str:pk>/', views.update_items, name='update_items'),
+    path('delete_item/<str:pk>/', views.delete_items, name="delete_item"),
 ]
--- stockmgt/views.py
+++ stockmgt/views.py
@@ -40,3 +40,12 @@ def update_items(request, pk):
         'header':'Update the Item details'
 	}
 	return render(request, 'add_item.html', context)
+
+def delete_items(request, pk):
+    
+    queryset = Stock.objects.get(id=pk)
+    if request.method == 'POST':
+        queryset.delete()
+        return redirect('list_items')
+    context={'item':queryset}
+    return render(request, 'delete_item.html', context)
