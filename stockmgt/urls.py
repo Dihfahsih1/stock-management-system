55e4f4abb4b306df89fc3aba0a3bf0c61d3486ca
Mugoya Dihfahsih <dihfahsihm@gmail.com>
2020-10-21 20:39:58 +0300

    work on update functionality

--- stockmgt/urls.py
+++ stockmgt/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('items-list', views.list_items, name='list_items'),
-    path('add-item', views.add_item, name='add_item')
+    path('add-item', views.add_item, name='add_item'),
+    path('add-item/<str:pk>/', views.update_items, name='update_items'),
 ]
