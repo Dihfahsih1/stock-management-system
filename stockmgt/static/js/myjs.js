32b002a871b0105dd29a260b7a671f61b9728517
Canon Innovations Ltd <dihfahsihm@gmail.com>
2020-10-23 14:52:54 +0300

    use pagination plug in to browse pages

--- stockmgt/static/js/myjs.js
+++ stockmgt/static/js/myjs.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-  
+    $('.table').paging({limit:10});
     $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
-   
+  
    
   });
+
+  
